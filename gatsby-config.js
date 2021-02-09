fd794eaaf110817d9e97500fd1467928f7acafe5
felipe linux <felipelullio@linux.com>
2021-02-09 11:16:56 -0300

    alteração do arquivo gatsby-config alteramos o título, descrição e autor, e apagamos uma imagem do src/imgs
    Ainda fazendo o cleanup starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My blog`,
+    description: `a blog about frotend developemnt and other cool stuff`,
+    author: `@myblog`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
