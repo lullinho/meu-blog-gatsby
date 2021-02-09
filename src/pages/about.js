2f3991f12a10c00dc0f6ce9070739fd2804afbe8
Felipe Lullio LINUX WSL <felipelullio@linux.com>
2021-02-09 11:49:53 -0300

    estrutura base para criar página dentro do gatsby

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+/*importar reat*/
+import React from 'react'
+
+/* criar const com nome da nossa pagina, Inicia maiuscula */
+const AboutPage = () => (
+    <h1>About Page</h1>
+)
+
+export default AboutPage
