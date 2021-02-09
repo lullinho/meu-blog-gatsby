d65162fd573e9c1841fb5a8ee5018bfdddbb91d1
Felipe Lullio LINUX WSL <felipelullio@linux.com>
2021-02-09 15:14:34 -0300

    usando o novo hook react useStaticQuery
    olhar código comentado

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,10 +1,40 @@
 import React from 'react'
-import {StaticQuery, graphql} from 'gatsby'
+import {useStaticQuery, graphql} from 'gatsby'
 
 //usar chaves porque vai ter que usar return, tratar dados primeiro
+//buscar dados do graphyql utilizando o hook do react useStaticQuery, separa dados e renderização, tratar dados primeiro, usar {} para return
+const Profile = () => {
+    const{//quais dados eu quero
+        site: { 
+            siteMetadata: { title, position, description },
+        }
+        //useStaticQuery retorna os dados acima(title, posi...)
+    } = useStaticQuery(graphql` 
+    query MysiteMedata{
+        site{
+            siteMetadata{
+                title
+                position
+                description
+                author
+            }
+        }
+    }
+    
+    `) 
+    //renderizar o componente
+    return(
+        <div class="Profile-wrapper">
+                    <h1>{title}</h1>
+                    <h2>{position}</h2>
+                    <p>{description}</p>
+            </div>
+    )
+}
 
+//StaticQuery é um componente react e dentro tem query e parte de renderização, no mesmo lugar trata dados e renderização.
 
-const Profile = () => (
+/*const Profile = () => (
     <StaticQuery
         query={graphql`
         query MysiteMedata{
@@ -32,6 +62,6 @@ const Profile = () => (
         )}
     />
     
-)
+)*/
 
 export default Profile
